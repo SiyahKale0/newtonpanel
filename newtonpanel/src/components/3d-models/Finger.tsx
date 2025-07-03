f4e8f8a4bd0cd8699683f8be45abf50549e6c2b2
Yunus Mirhan Aydınlı <mirhanayd@gmail.com>
2025-07-03 16:47:09 +0300

    oyun+istatistiği+hatalı+push

--- newtonpanel/src/components/3d-models/Finger.tsx
+++ newtonpanel/src/components/3d-models/Finger.tsx
@@ -1,16 +1,26 @@
 // src/components/3d-models/Finger.tsx
+"use client"
+
 import * as React from 'react';
 import { RoundedBox } from "@react-three/drei";
 import { selectedMaterial, skinMaterial } from './materials';
+import * as THREE from 'three';
 
 interface FingerProps {
     segments: { pos: [number, number, number]; rot: [number, number, number]; args: [number, number, number] }[];
     name: string;
     selected: boolean;
     onClick: (name: string) => void;
+    color?: THREE.Color | string;
 }
 
-export function Finger({ segments, name, selected, onClick }: FingerProps) {
+export function Finger({ segments, name, selected, color, onClick }: FingerProps) {
+    // Renge karar veriyoruz. Öncelik sırası:
+    // 1. Dışarıdan gelen özel renk (ısı haritası için)
+    // 2. Seçim rengi (kullanıcı tıkladığında)
+    // 3. Varsayılan ten rengi
+    const finalColor = color ? color : (selected ? selectedMaterial.color : skinMaterial.color);
+
     return (
         <group
             onClick={(e) => { e.stopPropagation(); onClick(name); }}
@@ -23,9 +33,15 @@ export function Finger({ segments, name, selected, onClick }: FingerProps) {
                         args={seg.args}
                         radius={0.04}
                         smoothness={4}
-                        material={selected ? selectedMaterial : skinMaterial}
                         castShadow
-                    />
+                    >
+                        {/* Materyali ve rengini doğrudan burada, deklaratif olarak ayarlıyoruz. */}
+                        <meshStandardMaterial
+                            color={finalColor}
+                            roughness={0.6}
+                            metalness={0.1}
+                        />
+                    </RoundedBox>
                 </group>
             ))}
         </group>
